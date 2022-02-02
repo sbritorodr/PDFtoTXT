11c627aad9e05a6fae8b4bae022daf8d9c64fd22
sbritorodr <sbritorodr@gmail.com>
2022-02-02 19:57:38 +0000

    Added translation module

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import shutil
 from pdf2image import convert_from_path
 from natsort import natsorted # because python string sorts is kinda bad tbh
+from deep_translator import GoogleTranslator
 
 title = str(input("Select the name of the file here ('foo.pdf)\n") or "foo.pdf")
 deleteCache = False
@@ -48,12 +49,35 @@ except:
 for element in listfiles:
     os.system('tesseract -l fra ' + './input/'+ element + ' ./output/' + element)
 
+#translate from gogle
+#there's a 5,000 character limit on google translator :(
+
+try: 
+    print('creating the output folder')
+    os.mkdir('./output_translated')
+except: 
+    print('output folder already generated')
+
+
+listtxt_trans = []
+for file in os.listdir('./output'):
+    if file.endswith('.txt'):
+        listtxt_trans.append('./output/' + file)
+i = 0
+for file in natsorted(listtxt_trans):
+    translated = GoogleTranslator(source='auto', target='es').translate_file(file)
+    output_translated = open(f'./output_translated/{i}.txt', 'w')
+    output_translated.write(translated)
+    output_translated.close() 
+    i = i+1
+
+
 
 #merge all into one txt
 listtxt = []
-for file in os.listdir('./output'):
+for file in os.listdir('./output_translated'):
     if file.endswith('.txt'):
-        listtxt.append('./output/' + file)
+        listtxt.append('./output_translated/' + file)
 with open('output_ocr_file.txt','wb') as wfd:
     for f in natsorted(listtxt): # Sorted all pages
         with open(f,'rb') as fd:
@@ -62,6 +86,8 @@ with open('output_ocr_file.txt','wb') as wfd:
 # delete all evidence
 if deleteCache:
     try: shutil.rmtree('./output')
+    except: print("Failed to delete the folder. Is already deleted or protected?")
+    try: shutil.rmtree('./output_translated')
     except: print("Failed to delete the folder. Is already deleted or protected?")
     try: shutil.rmtree('./input')
     except: print("Failed to delete the folder. Is already deleted or protected?")
@@ -75,9 +101,10 @@ if deleteCache:
 #translate from gogle
 #there's a 5,000 character limit on google translator :(
 
-# from deep_translator import GoogleTranslator
+#from deep_translator import GoogleTranslator
 
-# translated = GoogleTranslator(source='auto', target='es').translate_file('output_file.txt')
-# output_translated = open('output_file.txt')
-# output_translated.write(translated)
-# output_translated.close() 
+
+#translated = GoogleTranslator(source='auto', target='es').translate_file('output_file.txt')
+#output_translated = open('output_file.txt')
+#output_translated.write(translated)
+#output_translated.close() 
