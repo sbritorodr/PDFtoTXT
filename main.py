e0e583d1883357dc02a8eb989d8d282da2941332
sbritorodr <sbritorodr@gmail.com>
2022-02-04 16:02:32 +0000

    Added translate from all languages

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from natsort import natsorted # because python string sorts is kinda bad tbh
 from deep_translator import GoogleTranslator
 
 def USRInput():
-    USRInput.title = str(input("Select the name of the file here ('foo.pdf)\n") or "foo.pdf")
+    USRInput.title = str(input("Select the name of the file here ('input.pdf)\n") or "input.pdf")
     USRInput.output_ocr_file = str(input("Select your .txt output name (without extension) \n") or "output_ocr_file") + ".txt"
-    USRInput.deleteCache = False
+    USRInput.deleteCache = True
+    if not click.confirm('Delete folders?', default=True): # by default it will delete the folders
+        USRInput.deleteCache = False
+
     USRInput.translate = False
-    if click.confirm('Delete folders?'):
-        USRInput.deleteCache = True
-    if click.confirm(" Translate to spanish?"):
+    if click.confirm(" Translate?", default= False):
         USRInput.translate = True
 
     
@@ -27,7 +28,7 @@ def PDFtoPNG(): # convert pdf into multiple png's
     try: 
         print('creating the input folder')
         os.mkdir('./input')
-    except: 
+    except FileExistsError: 
         print('input folder already generated')
 
     images = convert_from_path(f'{USRInput.title}', 200)
@@ -64,6 +65,20 @@ def genOutputTrans():
     except: 
         print('output folder already generated')
 
+def TranslateOpt():
+    import inquirer as inq
+    questions = [
+        inq.List('lang',
+                message="Select which language you want to use",
+                choices=['spanish', 'english', 'french','italian', 'portuguese', 'german']
+            ),
+    ]
+    answers = inq.prompt(questions)
+    TranslateOpt.answers = answers
+    print(type(answers))
+    print(answers['lang'])
+
+
 def translatefromGoogle():
     listtxt_trans = []
     for file in os.listdir('./output'):
@@ -71,7 +86,7 @@ def translatefromGoogle():
             listtxt_trans.append('./output/' + file)
     i = 0
     for file in natsorted(listtxt_trans):
-        translated = GoogleTranslator(source='auto', target='es').translate_file(file)
+        translated = GoogleTranslator(source='auto', target=TranslateOpt.answers['lang']).translate_file(file)
         output_translated = open(f'./output_translated/{i}.txt', 'w')
         output_translated.write(translated)
         output_translated.close() 
@@ -103,15 +118,23 @@ def mergeALLtxtTranslated():
 # delete all evidence
 def rmEverything():
     if USRInput.deleteCache:
-        try: shutil.rmtree('./output')
-        except: print("Failed to delete the folder. Is already deleted or protected?")
-        try: shutil.rmtree('./output_translated')
-        except: print("Failed to delete the folder. Is already deleted or protected?")
-        try: shutil.rmtree('./input')
-        except: print("Failed to delete the folder. Is already deleted or protected?")
+        try: 
+            shutil.rmtree('./output')
+        except FileNotFoundError: 
+            print("Failed to delete the folder. Is already deleted or protected?")
+        try: 
+            shutil.rmtree('./output_translated')
+        except FileNotFoundError: 
+            print("Failed to delete the folder. Is already deleted or protected?")
+        try: 
+            shutil.rmtree('./input')
+        except FileNotFoundError: 
+            print("Failed to delete the folder. Is already deleted or protected?")
 
 def main():
     USRInput()
+    if USRInput.translate:
+        TranslateOpt()
     PDFtoPNG()
     fileSelector()
     genOutputfolder()
